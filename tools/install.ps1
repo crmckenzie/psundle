407c06e4b177a61ff246dfc105b4b09a398b3ef3
Chris McKenzie <cmckenzie@paraport.com>
2015-08-13 16:37:34 -0700

    enhanced Show-PsundleEnvironment; added install.ps1

---
+++ tools/install.ps1
@@ -0,0 +1,26 @@
+try
+{
+  $repository = "https://github.com/crmckenzie/psundle.git"
+
+  $modulesDirectory = ($env:PsModulePath.Split(';') | where {$_.contains($home)})
+
+  $psundleDirectory = Join-Path $modulesDirectory "Psundle"
+
+  if (Test-Path $psundleDirectory){
+    throw "Psundle directory already exists."
+  }
+
+  pushd $modulesDirectory
+  git clone $repository
+  popd
+
+  $psm1 = (gci $psundleDirectory "Psundle.psm1")[0]
+  Import-Module $psm1
+
+  write-host "Psundle installed." -ForegroundColor Green
+}
+catch {
+  write-host "Failed to install psundle." -ForegroundColor Red
+  write-host $_ -ForegroundColor Red
+
+}
